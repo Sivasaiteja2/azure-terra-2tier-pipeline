3f4975b4ed6666f774d0fde82ec57b67a4e2c8f7
Siva sai teja <gurramsai02@gmail.com>
2026-04-23 18:55:16 +0530

    Update backend.tf

--- azure-.2tier-terraform-python/backend.tf
+++ azure-.2tier-terraform-python/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "prasad_rgg"
-    storage_account_name = "prasadtfstateaccount434"
+    resource_group_name  = "sai_rg"
+    storage_account_name = "myaccount5155"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
